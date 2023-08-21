2269c49e5f901af72ee7541e8df8590ecf5edd74
Subham Mani <manisubham09@gmail.com>
2023-08-21 13:27:26 +0530

    modify module

--- index.js
+++ index.js
@@ -5,28 +5,31 @@ const emailManager = (function(){
     let copied = false;
     const emailId = 'manisubham09@gmail.com';
 
-    return {
-        copyEmailToClipboard: () => {
-            if(copied === false){
-                // setup visual elements
-        
-                copied = true;
-                copyIcon.classList.remove('copy');
-                copyIcon.classList.add('copied');
-        
-                // copy email to clipboard
-                navigator.clipboard.writeText(emailId);
-            }
-        },
-        resetCopy: () => {
-            if(copied){
-                copyIcon.classList.remove('copied');
-                copyIcon.classList.add('copy');
-                copied = false;
-            }
-        }
+    const copyEmailToClipboard = () => {
+      if(copied === false){
+        // setup visual elements
+
+        copied = true;
+        copyIcon.classList.remove('copy');
+        copyIcon.classList.add('copied');
+
+        // copy email to clipboard
+        navigator.clipboard.writeText(emailId);
+      }
     }
-  
+
+    const resetCopy = () => {
+      if(copied){
+          copyIcon.classList.remove('copied');
+          copyIcon.classList.add('copy');
+          copied = false;
+      }
+    }
+
+    return {
+        copyEmailToClipboard,
+        resetCopy
+    }
 })();
 
 gmailCard.addEventListener('click', () => {
